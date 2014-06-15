d8304ba007f2b799e45ae0b5351a1ddbc91336d5
Daniel Petkevich <daniel.petkevich@gmail.com>
2014-06-14 23:38:11 -0700

    first

---
+++ www/javascripts/application.js
@@ -0,0 +1,38 @@
+$( document ).ready(function() {
+
+//begin image capturing code
+	function cameraGetPicture() {
+	    navigator.device.capture.captureImage(captureSuccess, captureError, {limit: 1});
+	}
+
+	function captureSuccess(mediaFiles) {
+	    var i, len;
+	    
+	    var webView = new steroids.views.WebView("http://localhost/views/home/chooseSender.html");
+	  	steroids.layers.push({view: webView, navigationBar: false});
+	}
+
+
+	function captureError(error) {
+	    var msg = 'An error occurred during capture: ' + error.code;
+	    navigator.notification.alert(msg, null, 'Uh oh!');
+	}
+
+	//end image capturing code
+
+	//code to display sender footer
+
+	$('.personRow').hammer({}).on("tap",function(event){
+
+		
+		$(this).children('i').toggleClass('ion-ios7-circle-outline ion-ios7-circle-filled');
+
+
+	})
+
+}); //end document ready
+
+
+//make footer appear
+
+
